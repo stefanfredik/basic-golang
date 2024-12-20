9180b5cd20cd028fedbe18d77c4c2a67e0db805f
stefanfredik <stefanfredik@gmail.com>
2024-12-20 17:02:11 +0800

    pointer

---
+++ oop/oopstruct.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	Nama string
+	Age  int
+}
+
+func (p Person) Greet() {
+	fmt.Println("Hallo : ", p.Nama, ",Selamat sore!")
+}
+
+func main() {
+	person1 := Person{Nama: "Fredik", Age: 20}
+
+	person1.Greet()
+}
